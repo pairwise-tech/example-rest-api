4d172544d937fab4140ace9174b78ffae689d951
Sean Smith <sean.smith.2009@gmail.com>
2020-07-10 16:13:23 +0800

    Remove unused import

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-import { stat } from "fs";
 
 /** ===========================================================================
  * Setup Server
